0b9174aab5125b958a4f4fae5692da48800c314a
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-07-26 23:50:12 -0400

    Change start method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { ConsoleMap } from './ConsoleMap';
 import { clearScreenDown } from 'readline';
 
+export { ConsoleMap };
+
 // const lwss = `
 //  ■■■■
 // ■   ■
@@ -10,25 +12,31 @@ import { clearScreenDown } from 'readline';
 // lwss.shift();
 // TODO: Convert strings like this into Point arrays
 
-const app = new ConsoleMap();
-const seed = Number.parseInt(process.argv.pop()!) || 30;
+export function start() {
+    const app = new ConsoleMap();
+    const seed = Number.parseInt(process.argv.pop()!) || 30;
+    
+    app.seed(seed);
+    
+    app.on('cycle', function (cycleCount) {
+        app.render();
+    });
+    
+    setInterval(() => {
+        app.cycle();
+    }, 1000 / 33);
+    
+    process.on('SIGTERM', () => {
+        clearScreenDown(process.stdout);
+        process.exit(0);
+    });
+    
+    process.on('SIGINT', () => {
+        clearScreenDown(process.stdout);
+        process.exit(0);
+    });
+}
 
-app.seed(seed);
-
-app.on('cycle', function (cycleCount) {
-    app.render();
-});
-
-setInterval(() => {
-    app.cycle();
-}, 1000 / 33);
-
-process.on('SIGTERM', () => {
-    clearScreenDown(process.stdout);
-    process.exit(0);
-});
-
-process.on('SIGINT', () => {
-    clearScreenDown(process.stdout);
-    process.exit(0);
-});
+if (require.main === module) {
+    start();
+}
